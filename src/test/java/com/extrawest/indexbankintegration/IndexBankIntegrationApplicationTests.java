c65b5d92eb18ac11363bac1c902f0a96b4445220
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-06 02:29:52 +0300

    initial commit

---
+++ src/test/java/com/extrawest/indexbankintegration/IndexBankIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extrawest.indexbankintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexBankIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
