730a2f386d424eac41fd8b455cfc42ad66835feb
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-06 12:30:51 +0300

    added utility class

---
+++ src/main/java/com/extrawest/indexbankintegration/util/PathUtil.java
@@ -0,0 +1,9 @@
+package com.extrawest.indexbankintegration.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathUtil {
+    public static final String LOANS_ROOT_PATH = "/loans";
+    public static final String SEND_LOAN_REQUEST_PATH = "/sendLoan";
+}
