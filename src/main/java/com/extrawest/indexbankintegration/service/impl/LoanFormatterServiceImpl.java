e1ba484f7f267a703652ea39f12e4274f27d9643
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-19 20:12:46 +0300

    LoanFormatterServiceImpl.java refactored for using mapper

--- src/main/java/com/extrawest/indexbankintegration/service/impl/LoanFormatterServiceImpl.java
+++ src/main/java/com/extrawest/indexbankintegration/service/impl/LoanFormatterServiceImpl.java
@@ -2,28 +2,21 @@ package com.extrawest.indexbankintegration.service.impl;
 
 import com.extrawest.indexbankintegration.model.BankApplicationForm;
 import com.extrawest.indexbankintegration.model.dto.request.BankApplicationFormRequestDto;
+import com.extrawest.indexbankintegration.model.mapper.BankApplicationFormMapper;
 import com.extrawest.indexbankintegration.repository.BankApplicationFormRepository;
 import com.extrawest.indexbankintegration.service.LoanFormatterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor
 public class LoanFormatterServiceImpl implements LoanFormatterService {
 
     private final BankApplicationFormRepository bankApplicationFormRepository;
-
+    private final BankApplicationFormMapper bankApplicationFormMapper;
     @Override
     public BankApplicationForm loanRequestToBankApplicationForm(BankApplicationFormRequestDto bankApplicationFormRequestDto) {
-        BankApplicationForm bankApplicationForm = new BankApplicationForm();
-        bankApplicationForm.setAddress(bankApplicationFormRequestDto.getRegistrationCity() + " " + bankApplicationFormRequestDto.getRegistrationAddress());
-        bankApplicationForm.setDob(bankApplicationFormRequestDto.getDateOfBirth().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
-        bankApplicationForm.setInn(bankApplicationFormRequestDto.getTaxpayerCode());
-        bankApplicationForm.setLastName(bankApplicationFormRequestDto.getLastName());
-        bankApplicationForm.setName(bankApplicationFormRequestDto.getName());
-        bankApplicationForm.setPassportNum(bankApplicationFormRequestDto.getPassportSeries() + bankApplicationFormRequestDto.getPassportNumber());
+        BankApplicationForm bankApplicationForm = bankApplicationFormMapper.toModel(bankApplicationFormRequestDto);
         return bankApplicationFormRepository.save(bankApplicationForm);
     }
 }
